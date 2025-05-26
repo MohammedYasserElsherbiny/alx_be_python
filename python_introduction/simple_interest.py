d6a059774184c74951a3565b2788a5f74627d814
MohammedYasserElsherbiny <98979716+MohammedYasserElsherbiny@users.noreply.github.com>
2025-05-26 20:19:46 +0300

    update simple

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,6 @@
-initial_amount = 1000  
-interest_rate = 0.05   
-duration_years = 3     
-
-simple_interest = interest_rate * duration_years * initial_amount
-simple_interest = int(simple_interest)
-
-print("The simple interest is:", simple_interest)
+principal = 1000  
+rate = 0.05  
+time = 3  
+interest = rate * time * principal 
+interest = int(interest)
+print("The simple interest is:", interest)
